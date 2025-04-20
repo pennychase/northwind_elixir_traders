50e8f3054bf930ac67338735fac797ed9e914fe9
Penny Chase <mpchase@mac.com>
2025-04-20 13:16:22 -0400

    Adding column check constraint for field length in alter-categories migration

--- priv/repo/migrations/20250420140143_alter_categories.exs
+++ priv/repo/migrations/20250420140143_alter_categories.exs
@@ -3,10 +3,10 @@ defmodule NorthwindElixirTraders.Repo.Migrations.AlterCategories do
 
   def change do
     create table(:newcats, primary_key: false) do
-      add :id, :integer, primary_key: true, start_value: 999_983, increment: 13, comment: "Deliberately weird autoincrementing integer primary-key values"
-      add :name, :string, null: false, size: 50, comment: "Required; max of original data is 14"
-      add :description, :string, size: 100, comment: "(Optional; max of original data is 58"
-      timestamps(type: :utc_datetime)
+      add :id, :integer, primary_key: true, start_value: 999_983, increment: 13
+      add :name, :string, null: false, check: %{name: "name_maxlength_constraint", expr: "length(name) <= 50"}
+      add :description, :string, check: %{name: "description", expr: "length(description) <= 100"}
+      timestamps(type: :utc_database)
     end
 
     execute("INSERT INTO newcats (id, name, description, inserted_at, updated_at) SELECT id, name, description, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP FROM categories")
