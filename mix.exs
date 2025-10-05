7371d39abf5bf268e87b4be4d209580681377f54
Penny Chase <mpchase@mac.com>
2025-10-05 16:11:16 -0400

    Initial repo

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule NorthwindElixirTraders.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :northwind_elixir_traders,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {NorthwindElixirTraders.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ecto_sqlite3, "~> 0.22.0"}
+    ]
+  end
+end
