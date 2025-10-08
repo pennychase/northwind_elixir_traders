56087d07824ff7dfef92553ce18d5d339d846ee7
Penny Chase <mpchase@mac.com>
2025-10-08 18:39:28 -0400

    Chapter 5 - Created Employee schema and added changesets to Category and Employye to validate values of a table record

--- lib/northwind_elixir_traders/employee.ex
+++ lib/northwind_elixir_traders/employee.ex
@@ -1,5 +1,51 @@
 defmodule NorthwindElixirTraders.Employee do
+  use Ecto.Schema
+  import Ecto.Changeset
 
-  defstruct [:last_name, :first_name, :birth_date, :photo, :notes]
+  @name_mxlen 50
+  @notes_mxlen 500
+  @age_mx 100
+  @age_mn 15
+
+  schema "employees" do
+    field(:last_name, :string)
+    field(:first_name, :string)
+    field(:birth_date, :date)
+    field(:photo, :string)
+    field(:notes, :string)
+
+    timestamps(type: :utc_datetime)
+  end
+
+  def changeset(data, params \\ %{}) do
+    permitted = [:last_name, :first_name, :birth_date, :photo, :notes]
+    required = [:last_name, :first_name, :birth_date]
+
+    data
+    |> cast(params, permitted)
+    |> validate_required(required)
+    |> validate_length(:last_name, max: @name_mxlen)
+    |> validate_length(:first_name, max: @name_mxlen) 
+    |> validate_length(:notes, max: @notes_mxlen)
+    |> validate_age_range(:birth_date, min: @age_mn, max: @age_mx)
+  end
+
+  defp validate_age_range(changeset, field, [min: age_mn, max: age_mx] = opts) 
+      when is_atom(field) and is_list(opts) do
+    case {changeset.errors[field], get_field(changeset, field)} do
+      {nil, field_value} when not is_nil(field_value) ->
+        within_range? = field_value
+          |> Date.diff(Date.utc_today())
+          |> then(&Kernel.and(Kernel.>(&1, -age_mx * 365), Kernel.<(&1, -age_mn * 365)))
+        if not within_range? do
+          add_error(changeset, field, 
+            "date not within the specified range between '%{min}' and '%{max}' years ago",
+            min: age_mn, max: age_mx, validation: :age_range)
+        else
+          changeset
+        end
+      _ -> changeset
+    end
+  end
   
 end
