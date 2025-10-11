9cfe13d4e2518b052bf1d85e3f4f33497254adac
Penny Chase <mpchase@mac.com>
2025-10-11 15:06:37 -0400

    Created 1:M association between caetories and products tables

--- .iex.exs
+++ .iex.exs
@@ -3,5 +3,7 @@ import Ecto.Query
 alias NorthwindElixirTraders.Category
 alias NorthwindElixirTraders.Repo
 alias NorthwindElixirTraders.Employee
+alias NorthwindElixirTraders.Supplier
+alias NorthwindElixirTraders.Product
 
 IEx.configure(inspect: [charlists: :as_lists])
---
+++ lib/northwind_elixir_traders/product.ex
@@ -0,0 +1,30 @@
+defmodule NorthwindElixirTraders.Product do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias NorthwindElixirTraders.{Category, Validations}
+
+  @name_mxlen 50
+
+  schema "products" do
+    field(:name, :string)
+    field(:unit, :string)
+    field(:price, :float)
+    field(:category_id, :integer)
+
+    timestamps(type: :utc_datetime)
+  end
+
+  def changeset(data, params \\ %{}) do
+    permitted = [:name, :unit, :price, :category_id]
+    required = permitted
+
+    data
+    |> cast(params, permitted)
+    |> validate_required(required)
+    |> validate_length(:name, max: @name_mxlen)
+    |> Validations.validate_foreign_key_id(Category, :category_id)
+    |> unique_constraint([:name])
+  end
+  
+end
