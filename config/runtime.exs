a4ef4210f98ed0937ee023f1bf9f04dc5c50455c
Penny Chase <pc@mitre.org>
2025-10-26 17:34:14 -0400

    Changed Sqlite DB environmental variable

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,6 +1,6 @@
 import Config
 
-database_home = System.get_env("SQLITE_DB") || ""
+database_home = System.get_env("SQLITE_DB_DIR") || ""
 
 config :northwind_elixir_traders, NorthwindElixirTraders.Repo,
  database: database_home <> "northwind_elixir_traders_repo.db"
