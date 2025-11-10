86a4ba1161958c059c5f60e0192139bfb9c51107
Penny Chase <mpchase@mac.com>
2025-11-10 18:54:20 -0500

    Chapter 12 - used many_to_many to model the many to may relationship betyween Order and Product

--- lib/northwind_elixir_traders/product.ex
+++ lib/northwind_elixir_traders/product.ex
@@ -2,7 +2,7 @@ defmodule NorthwindElixirTraders.Product do
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias NorthwindElixirTraders.{Category, Validations, Supplier, OrderDetail}
+  alias NorthwindElixirTraders.{Category, Validations, Supplier, OrderDetail, Order}
 
   @name_mxlen 50
 
@@ -13,7 +13,8 @@ defmodule NorthwindElixirTraders.Product do
     field(:category_id, :integer)
     belongs_to(:category, Category, define_field: false)  # option allows us to define the foreign key manually
     belongs_to(:supplier, Supplier)
-    has_many(:order_details, OrderDetail)
+    # has_many(:order_details, OrderDetail)
+    many_to_many(:orders, Order, join_through: OrderDetail)
 
     timestamps(type: :utc_datetime)
   end
