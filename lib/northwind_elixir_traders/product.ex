53bff1a0afe069b98e995406e1eb17aaececea2c
Penny Chase <mpchase@mac.com>
2025-11-10 14:24:00 -0500

    Chapter 12 - Modeled OrderDetail with belongs_to and has_many

--- lib/northwind_elixir_traders/product.ex
+++ lib/northwind_elixir_traders/product.ex
@@ -2,7 +2,7 @@ defmodule NorthwindElixirTraders.Product do
   use Ecto.Schema
   import Ecto.Changeset
 
-  alias NorthwindElixirTraders.{Category, Validations, Supplier}
+  alias NorthwindElixirTraders.{Category, Validations, Supplier, OrderDetail}
 
   @name_mxlen 50
 
@@ -13,6 +13,7 @@ defmodule NorthwindElixirTraders.Product do
     field(:category_id, :integer)
     belongs_to(:category, Category, define_field: false)  # option allows us to define the foreign key manually
     belongs_to(:supplier, Supplier)
+    has_many(:order_details, OrderDetail)
 
     timestamps(type: :utc_datetime)
   end
