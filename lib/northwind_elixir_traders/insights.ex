d5a463f910d43035cf64fa305b52303fd1074c56
Penny Chase <mpchase@mac.com>
2025-11-15 07:23:41 -0500

    Chapter 14 - parallel benchmarking

--- lib/northwind_elixir_traders/insights.ex
+++ lib/northwind_elixir_traders/insights.ex
@@ -1,6 +1,10 @@
 defmodule NorthwindElixirTraders.Insights do
   import Ecto.Query
+  alias NorthwindElixirTraders.Insights
   alias NorthwindElixirTraders.{Repo, Product, Order, OrderDetail}
+
+  @max_concurrency System.schedulers_online()
+  @timeout 10_000
 
   def query_order_details_by_order(order_id) do
     OrderDetail
@@ -27,5 +31,31 @@ defmodule NorthwindElixirTraders.Insights do
     |> query_order_total_value()
     |> Repo.one()
   end
-  
+
+  def calculate_total_value_of_orders(orders, opts \\ [max_concurrency: @max_concurrency])
+      when is_list(orders) and is_list(opts) do
+    mc =
+      if Keyword.has_key?(opts, :max_concurrency) do
+        Keyword.get(opts, :max_concurrency)
+      else
+        @max_concurrency
+      end
+
+    Task.async_stream(orders, &calculate_order_value/1, ordered: false, timeout: @timeout, max_concurreny: mc)
+    |> Enum.to_list()
+    |> Enum.reduce(0.0, fn {_status, value}, acc -> acc + value end)
+  end
+
+  # Benchmarking parallelism
+  # Run in IEx (compute time per # processor and times relative to one processor):
+  # x = 1..(2*System.schedulers_online()) |> Enum.map(&{&1, Insights.parallel_benchmark(&1, 1000, orders)})
+  # x_rel = Enum.map(x, fn {mc, t} -> {mc, t / Enum.max(Keyword.values(x)) |> Float.round(3)} end)
+
+  def parallel_benchmark(mc, reps, orders) do
+    :timer.tc(fn -> 
+      Enum.map(1..1000, fn _ -> Insights.calculate_total_value_of_orders(orders, max_concurrency: mc) end)
+    end)
+    |> elem(0)
+    |> Kernel./(reps)
+  end
 end
